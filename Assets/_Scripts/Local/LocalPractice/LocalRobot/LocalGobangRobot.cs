268a61d51209e10c9c75652e65ea2738bf6f12b8
AshScops <1349143251@qq.com>
2023-10-06 00:09:51 +0800

    实现基于权值的对弈算法（未验证）

--- Assets/_Scripts/Local/LocalPractice/LocalRobot/LocalGobangRobot.cs
+++ Assets/_Scripts/Local/LocalPractice/LocalRobot/LocalGobangRobot.cs
@@ -10,6 +10,7 @@ namespace Tabletop.Local
         public LocalGobangRobot(GoChessColor color, LocalGoChessBasket chessBasket, LocalMapObj map)
         {
             InitValueMap();
+            InitWeights(map.Grids.Width, map.Grids.Height);
 
             m_robotColor = color;
             m_chessBasket = chessBasket;
@@ -30,51 +31,125 @@ namespace Tabletop.Local
 
         public IEnumerator OnTurnToRobot()
         {
-            var xMax = m_map.Grids.Width;
-            var zMax = m_map.Grids.Height;
-            var x = Random.Range(0, xMax);
-            var z = Random.Range(0, zMax);
-            while (m_map.Grids[x, z].Occupied)
+            #region 对弈算法未实现时的临时替代
+            //var xMax = m_map.Grids.Width;
+            //var zMax = m_map.Grids.Height;
+            //var x = Random.Range(0, xMax);
+            //var z = Random.Range(0, zMax);
+            //while (m_map.Grids[x, z].Occupied)
+            //{
+            //    x = Random.Range(0, xMax);
+            //    z = Random.Range(0, zMax);
+            //}
+            //var piece = m_chessBasket.Get(m_robotColor);
+            //m_map.Grids[x, z].AttachArea.Attach(piece);
+            #endregion
+
+            #region 权值法
+
+            m_maxWeight = 0;
+            m_maxX = 0;
+            m_maxY = 0;
+
+            for (int i = 0; i < m_map.Grids.Width; i++)
             {
-                yield return null;
-                x = Random.Range(0, xMax);
-                z = Random.Range(0, zMax);
+                yield return null;//每帧遍历19格，最坏情况下的每帧总计算次数=>19*8*5 = 760次
+
+                for (int j = 0; j < m_map.Grids.Height; j++)
+                {
+                    if (m_map.Grids[i, j].Occupied) continue;//仅计算当前可落子的区域，已落子的区域直接跳过
+
+                    m_weights[i, j] = 0;
+                    for (int k = 0; k < m_directions.Length; k++)
+                    {
+                        m_weights[i, j] += GetWeight(m_map.Grids[i, j], m_directions[k]);
+                    }
+                    ///最后的权值最大点即为AI落子点
+                    if(m_maxWeight < m_weights[i, j])
+                    {
+                        m_maxWeight = m_weights[i, j];
+                        m_maxX = i;
+                        m_maxY = j;
+                    }
+                }
             }
+
             var piece = m_chessBasket.Get(m_robotColor);
-            m_map.Grids[x, z].AttachArea.Attach(piece);
+            m_map.Grids[m_maxX, m_maxY].AttachArea.Attach(piece);
+
+            #endregion
         }
 
-        private void CalculateValue()
+
+        /// <summary>
+        /// 返回目标点在给定方向上的权值
+        /// </summary>
+        /// <param name="gridData">保证目标点尚未落子即可</param>
+        /// <param name="direction">八个方向</param>
+        /// <returns></returns>
+        private int GetWeight(LocalGridData gridData, Vector2Int direction)
         {
+            string situation = "";
+            Vector2Int pos = new Vector2Int(gridData.X, gridData.Z);
+            while (pos.x >= 0 && pos.x < m_map.Grids.Width && pos.y >= 0 && pos.y < m_map.Grids.Height)
+            {
+                if (!m_map.Grids[pos.x, pos.y].Occupied) break;
 
-
-
+                if (m_map.Grids[pos.x, pos.y].DragObject is LocalGoChessPiece piece)
+                {
+                    situation += ((int)piece.VirtualColor).ToString();
+                }
+            }
+            Debug.Log("m_weightHashMap[situation:" + m_weightHashMap[situation]);
+            return m_weightHashMap[situation];
         }
 
-        private Dictionary<string, int> m_valueMap;
+        private int m_maxWeight = 0;
+        private int m_maxX = 0;
+        private int m_maxY = 0;
+
+        private EasyGrid<int> m_weights;
+        private void InitWeights(int width, int height)
+        {
+            m_weights = new EasyGrid<int>(width, height);
+        }
+
+        private Vector2Int[] m_directions = new Vector2Int[]
+        {
+            new Vector2Int(-1, 0),
+            new Vector2Int(-1, 1),
+            new Vector2Int(0, 1),
+            new Vector2Int(1, 1),
+            new Vector2Int(1, 0),
+            new Vector2Int(1, -1),
+            new Vector2Int(0, -1),
+            new Vector2Int(-1, -1),
+        };
+
+        private Dictionary<string, int> m_weightHashMap;
         private void InitValueMap()
         {
-            m_valueMap = new Dictionary<string, int>();
+            m_weightHashMap = new Dictionary<string, int>();
 
             //空-0；黑-1；白-2
-            m_valueMap["1"] = 20;
-            m_valueMap["11"] = 400;
-            m_valueMap["111"] = 500;
-            m_valueMap["1111"] = 8000;
-            m_valueMap["2"] = 20;
-            m_valueMap["22"] = 400;
-            m_valueMap["222"] = 500;
-            m_valueMap["2222"] = 8000;
+            m_weightHashMap["1"] = 20;
+            m_weightHashMap["11"] = 400;
+            m_weightHashMap["111"] = 500;
+            m_weightHashMap["1111"] = 8000;
+            m_weightHashMap["2"] = 20;
+            m_weightHashMap["22"] = 400;
+            m_weightHashMap["222"] = 500;
+            m_weightHashMap["2222"] = 8000;
 
-            m_valueMap["12"] = 8;
-            m_valueMap["112"] = 80;
-            m_valueMap["1112"] = 450;
-            m_valueMap["11112"] = 8000;
+            m_weightHashMap["12"] = 8;
+            m_weightHashMap["112"] = 80;
+            m_weightHashMap["1112"] = 450;
+            m_weightHashMap["11112"] = 8000;
 
-            m_valueMap["21"] = 8;
-            m_valueMap["221"] = 80;
-            m_valueMap["2221"] = 450;
-            m_valueMap["22221"] = 8000;
+            m_weightHashMap["21"] = 8;
+            m_weightHashMap["221"] = 80;
+            m_weightHashMap["2221"] = 450;
+            m_weightHashMap["22221"] = 8000;
         }
 
 
